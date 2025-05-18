0c40c40a93cee8500f712a6f4427063514040a40
mzharkynbaiuly <marlen.ormonbaev05@gmail.com>
2025-05-18 06:46:27 -0600

    initial commiit

---
+++ src/main/java/kg/alatoo/studentcouncil/StudentCouncilApplication.java
@@ -0,0 +1,13 @@
+package kg.alatoo.studentcouncil;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudentCouncilApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(StudentCouncilApplication.class, args);
+    }
+
+}
---
+++ src/test/java/kg/alatoo/studentcouncil/StudentCouncilApplicationTests.java
@@ -0,0 +1,13 @@
+package kg.alatoo.studentcouncil;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudentCouncilApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
